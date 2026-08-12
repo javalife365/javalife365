3673b44828ef6b45271ba8b91da9e4227533f5ba
javalife365 <java.life.365@gmail.com>
2026-08-12 06:32:06 -0400

    code fix

--- javalife365api/src/main/java/com/javalife365/javalife365api/service/CustomUserDetailsService.java
+++ javalife365api/src/main/java/com/javalife365/javalife365api/service/CustomUserDetailsService.java
@@ -25,7 +25,7 @@ public class CustomUserDetailsService  implements UserDetailsService {
         return  new User(
                 appUser.getPhoneNumber(),
                 "",
-                List.of(new SimpleGrantedAuthority(appUser.getRole().toString()))
+                List.of(new SimpleGrantedAuthority(appUser.getRole().name()))
         );
     }
 }
